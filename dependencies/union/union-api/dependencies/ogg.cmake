581fcea618da9f3eb6287e6b456d186a008e3bb7
Igor <dancigor@gmail.com>
2026-01-05 00:25:58 +0400

    reorganization project structure

--- dependencies/union/union-api/dependencies/ogg.cmake
+++
@@ -1,4 +0,0 @@
-set(BUILD_TESTING OFF)
-set(INSTALL_DOCS OFF)
-
-add_subdirectory(ogg)
--- dependencies/union/update.bat
+++
@@ -1,9 +0,0 @@
-cd gothic-api
-git fetch origin
-git checkout -d origin/main
-cd ..
-
-cd union-api
-git fetch origin
-git checkout -d origin/main
-cd ..
